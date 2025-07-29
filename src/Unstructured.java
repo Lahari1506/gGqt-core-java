e39601a5bd9aeba0e9d6e4d10b5c5ee49da01b50
laharinallamilli <laharinallamilli0@gmail.com>
2025-07-29 22:15:56 +0530

    Unstructured array

---
+++ src/Unstructured.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Unstructured{
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("nter the Students count:");
+		int n=in.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println("Enter Marks of student " +(i+1)+ " is");
+			arr[i]=in.nextInt();
+		}//System.out.print("-------------------");
+		System.out.println();
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println("The marks of studnet of "+(i+1)+" is " +arr[i]);
+		}
+		
+
+	}
+
+}
