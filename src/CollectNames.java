d2343e7cb6c733907205e51a337028a561c412b4
laharinallamilli <laharinallamilli0@gmail.com>
2025-07-29 22:19:41 +0530

    Arrays

---
+++ src/CollectNames.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+class Array3{
+	 Scanner sc= new Scanner(System.in);
+	 String arr[][];
+	 void createArray() {
+		 System.out.println("enter class count");
+		 int cl=sc.nextInt();
+		 System.out.println("enter Student count for each class");
+		 int stu=sc.nextInt();
+		 arr=new String[cl][stu];
+		 System.out.println("Array is created");
+		 System.out.println("-------------------------");
+	 }
+	 void collectData() {
+		 for (int i=0;i<arr.length;i++) {
+			 System.out.println("inside class number"+(i+1));
+			 for(int j=0;j<arr[i].length;j++) {
+				 System.out.println("enter name of the student no:"+(j+1));
+				 arr[i][j]=sc.next();
+			 }
+		 }
+		 System.out.println("Data collected");
+		 System.out.println("---------------------------");
+	 }
+	 void displayData() {
+		 for (int i=0;i<arr.length;i++) {
+			 System.out.println("inside class number"+(i+1));
+			 for(int j=0;j<arr[i].length;j++) {
+				 System.out.println("enter name of the student no:"+(j+1)+" is "+arr[i][j]);
+				 
+			 }
+		 }
+System.out.println("-------------------------------");
+	 }
+ }
+public class CollectNames {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Array3 cn=new Array3();
+	  cn.createArray();
+	  cn.collectData();
+	  cn.displayData();
+	  
+
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class Structured {
+		int arr[];
+		Scanner in=new Scanner(System.in);
+		void createArray()
+		{
+			System.out.print("Enter the students count: ");
+			int n=in.nextInt();
+			arr=new int[n];
+			System.out.println("array Created");
+		}
+		void collectData()
+		{
+			for(int i=0;i<arr.length;i++){
+			System.out.println("Enter Marks of the student " +(i+1)+ " is ");
+			arr[i]=in.nextInt();
+			}
+		}
+		//System.out.print("Data is collected");
+		void displayData()
+		{
+			for(int i=0;i<arr.length;i++)
+			{
+			System.out.println("Marks of student"+(i+1)+ " is "+arr[i]);
+			}
+	}
+
+}
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Structured a=new Structured();
+		a.createArray();
+		a.collectData();
+		a.displayData();
+	}
+}
